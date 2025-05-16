88f57db0d24ed30e254434355b2dd456c0e6c67c
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-05-16 21:43:53 +0530

    Update WorkExperience.js

--- src/app/components/WorkExperience/WorkExperience.js
+++ src/app/components/WorkExperience/WorkExperience.js
@@ -12,7 +12,7 @@ const WorkExperience = () => {
               <FaBriefcase size={24} color="#007bff" /> {/* Job Icon */}
             </div>
             <div className={styles.timelineContent}>
-              <h3 className={styles.title}>Business Operations Associate at Nxtwave</h3>
+              <h3 className={styles.title}>Software Engineer at Nxtwave</h3>
               <p className={styles.timePeriod}>March 2024 - Present</p>
               <p className={styles.description}>Worked on various business operations and collaborated with teams.
                 I manage and oversee all internal business operations, ensuring that various departments within the organization work seamlessly to achieve business objectives</p>
